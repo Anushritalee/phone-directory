a249759e9e419607ec3b9e25de14d2efcd06ace2
Anushrita Anshul Lee <66158280+Anushritalee@users.noreply.github.com>
2021-03-26 20:44:39 +0530

    Styled ‘Header’ (Inline Styling)

---
+++ Header.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Header= function(){
+    const headerStyle = {
+        textAlign: 'center',
+        padding: 20,
+        background: '#000',
+        color: '#fff',
+        textTransform: 'uppercase'
+     };
+    return(
+        <div className="header" style={headerStyle}>
+            Phone directory
+        </div>
+    )
+}
+export default Header;
