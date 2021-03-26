cff2ea8838d28acad7755341a1798368e8cffd1c
Anushrita Anshul Lee <66158280+Anushritalee@users.noreply.github.com>
2021-03-26 20:49:01 +0530

    Created Skeleton of Home Page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 
 
-function App() {
+import { render } from "@testing-library/react";
+import { Component } from "react";
+
+class App extends Component{
+render(){
   return (
-    <div >
-     phone directory
+    <div>
+      {/* <div className="Headers">
+      phone directory
     </div>
+    <button> Add </button>
+    <div>
+      <span>Name</span><br/>
+      <span>Phone</span>
+  </div>*/ }
+  <label htmlfor="Name">Name:</label>
+  <input id="name" type="text" placeholder="type here"/>
+    </div>
+   
+
   );
 }
-
+}
 export default App;
